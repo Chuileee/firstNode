2febe94ac22c01ce967433a0e2aa3b3ecf035b2b
Chuileee <leechui618@gmail.com>
2023-05-24 20:18:35 -0400

    nodemon

--- main.js
+++ main.js
@@ -19,16 +19,44 @@ const server = http.createServer((req,res) => {
     console.log(req.url,req.method);
 
     const url =req.url;
-    if(url ==='/google'){
+    const method = req.method;
+    if(url ==='/'){
         res.setHeader('Content-Type','text/html');
         res.write("<html>");
         res.write("<head><title>My First Node app</title></head>");
-        res.write("<body><h1>go to google web</h1></body>");
+
+        res.write('<body><form action="/message" method="post"><input type="test" name = "message"/><button type="submit">submit</form></body>');
         res.write("</html>");
         res.end();
 
 
     }
+
+    if(url ==='/message' && method === 'POST'){
+        //write code to receive data and parse that data
+        const body =[];
+        req.on('data',(chunk)=> {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        //parse the data
+        req.on('end',()=>{
+            const.parseBody = Buffer.concat(body).toString();
+            console.log(parseBody); //parseBody = key/value
+            const message = parseBody.split('=')[1];
+            console.log(message);
+            fs.writeFileSync('message.txt',message);
+        });
+
+
+
+        fs.writeFileSync('message.txt','Lee')
+        res.statusCode =302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+
     res.setHeader('Content-Type','text/html');
     res.write("<html>");
     res.write("<head><title>My First Node app</title></head>");
@@ -36,6 +64,8 @@ const server = http.createServer((req,res) => {
     res.write("</html>");
     res.end();
 });
+
+
 
 
 server.listen(3000);
